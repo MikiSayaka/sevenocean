400da79e226436f2bba89c85c33db105718c5d8c
Miki Sayaka <a8965128@gmail.com>
2017-12-24 08:04:33 +0000

    Add insert function.

--- modules/dataHandle.js
+++ modules/dataHandle.js
@@ -13,12 +13,55 @@ exports.getAll = function(done) {
   });
 };
 
+exports.insertContainerInfo = function(_containerInfo, _done) {
+  var _values = [
+    _containerInfo.container_no,
+    _containerInfo.type,
+    _containerInfo.hds_lease_ref,
+    _containerInfo.loading_port,
+    _containerInfo.lessor_depot,
+    _containerInfo.transaction_time,
+    _containerInfo.lease_company,
+    _containerInfo.hds_depot,
+    _containerInfo.depot,
+    _containerInfo.shipper,
+    _containerInfo.forwardef,
+    _containerInfo.booking_ref_no,
+    _containerInfo.hauler_co,
+    _containerInfo.release_ref_no,
+    _containerInfo.full_empty,
+    _containerInfo.seal_no,
+    _containerInfo.owner_type,
+    _containerInfo.physical_status,
+    _containerInfo.destination,
+    _containerInfo.vessel,
+    _containerInfo.voyage,
+    _containerInfo.b_l_no,
+    _containerInfo.consignee,
+    _containerInfo.notify,
+    _containerInfo.weight,
+    _containerInfo.bay,
+    _containerInfo.slot,
+    _containerInfo.carrier_type,
+    _containerInfo.discharge_port,
+    _containerInfo.carrier_id,
+    _containerInfo.acceptance_ref,
+    _containerInfo.container_status,
+    _containerInfo.data_seq
+  ];
+  db.get().query('INSERT INTO CONTAINER_INFO (CONTAINER_NO, CONTAINER_TYPE, HDS_LEASE_REF, LOADING_PORT, LESSOR_DEPOT, TRANSACTION_TIME, LEASE_COMPANY, HDS_DEPOT, DEPOT, SHIPPER, FORWARDEF, BOOKING_REF_NO, HAULER_CO, RELEASE_REF_NO, FULL_EMPTY, SEAL_NO, OWNER_TYPE, PHYSICAL_STATUS, DESTINATION, VESSEL, VOYAGE, B_L_NO, CONSIGNEE, NOTIFY, WEIGHT, BAY, SLOT, CARRIER_TYPE, DISCHARGE_PORT, CARRIER_ID, ACCEPTANCE_REF, CONTAINER_STATUS, DATA_SEQ ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ', _values, function(_err, _result){
+    if (_err) {
+      return _done('Indsert error.');
+    }
+    _done(null, _result);
+  });
+}
+
 exports.test = function(done) {
   db.get().query('SELECT 1 + 1 AS SOLUTION;', function(err, rows){
     if (err) {
       return done(err);
     }
-    console.log(rows);
     done(null, rows);
   });
 };
--- routes/paths.js
+++ routes/paths.js
@@ -12,17 +12,42 @@ router.get('/', function(req, res, next) {
 /* POST data insert. */
 router.post('/', function(req, res, next) {
   var _url = req.baseUrl;
-
-  //  console.log(_url);
-  //  console.log('test');
-  //  console.log(req.body.container_no);
-  //  console.log(req.body.type);
-  //  console.log(req.body.hds);
-  //  console.log(req.body.loading_port);
-  //  console.log(req.body.lessor_depot);
-  //  console.log(req.body.transaction_time);
-  //  console.log(req.body.hds_depot);
+  var _containerInfo = new Object();
   
+  _containerInfo.container_no = (req.body.container_no == undefined) ? '' : req.body.container_no;
+  _containerInfo.type = (req.body.type == undefined) ? '' : req.body.type;
+  _containerInfo.hds_lease_ref = (req.body.hds_lease_ref == undefined) ? '' : req.body.hds_lease_ref;
+  _containerInfo.loading_port = (req.body.loading_port == undefined) ? '' : req.body.loading_port;
+  _containerInfo.lessor_depot = (req.body.lessor_depot == undefined) ? '' : req.body.lessor_depot;
+  _containerInfo.transaction_time = (req.body.transaction_time == undefined) ? '' : req.body.transaction_time;
+  _containerInfo.lease_company = (req.body.lease_company == undefined) ? '' : req.body.lease_company;
+  _containerInfo.hds_depot = (req.body.hds_depot == undefined) ? '' : req.body.hds_depot;
+  _containerInfo.depot = (req.body.depot == undefined) ? '' : req.body.depot;
+  _containerInfo.shipper = (req.body.shipper == undefined) ? '' : req.body.shipper;
+  _containerInfo.forwardef = (req.body.forwardef == undefined) ? '' : req.body.forwardef;
+  _containerInfo.booking_ref_no = (req.body.booking_ref_no == undefined) ? '' : req.body.booking_ref_no;
+  _containerInfo.hauler_co = (req.body.hauler_co == undefined) ? '' : req.body.hauler_co;
+  _containerInfo.release_ref_no = (req.body.release_ref_no == undefined) ? '' : req.body.release_ref_no;
+  _containerInfo.full_empty = (req.body.full_empty == undefined) ? '' : req.body.full_empty;
+  _containerInfo.seal_no = (req.body.seal_no == undefined) ? '' : req.body.seal_no;
+  _containerInfo.owner_type = (req.body.owner_type == undefined) ? '' : req.body.owner_type;
+  _containerInfo.physical_status = (req.body.physical_status == undefined) ? '' : req.body.physical_status;
+  _containerInfo.destination = (req.body.destination == undefined) ? '' : req.body.destination;
+  _containerInfo.vessel = (req.body.vessel == undefined) ? '' : req.body.vessel;
+  _containerInfo.voyage = (req.body.voyage == undefined) ? '' : req.body.voyage;
+  _containerInfo.b_l_no = (req.body.b_l_no == undefined) ? '' : req.body.b_l_no;
+  _containerInfo.consignee = (req.body.consignee == undefined) ? '' : req.body.consignee;
+  _containerInfo.notify = (req.body.notify == undefined) ? '' : req.body.notify;
+  _containerInfo.weight = (req.body.weight == undefined) ? '' : req.body.weight;
+  _containerInfo.bay = (req.body.bay == undefined) ? '' : req.body.bay;
+  _containerInfo.slot = (req.body.slot == undefined) ? '' : req.body.slot;
+  _containerInfo.carrier_type = (req.body.carrier_type == undefined) ? '' : req.body.slot;
+  _containerInfo.discharge_port = (req.body.discharge_port == undefined) ? '' : req.body.discharge_port;
+  _containerInfo.carrier_id = (req.body.carrier_id == undefined) ? '' : req.bod.carrier_id;
+  _containerInfo.acceptance_ref = (req.body.acceptance_ref == undefined) ? '' : req.bod.acceptance_ref;
+  _containerInfo.container_status = (req.body.container_status == undefined) ? '' : req.bod.container_status;
+  _containerInfo.data_seq = '';
+
   data.test(function(_a, _b){
     var _restuleArr = _b;
     for (var _key in _restuleArr) {
